eee4d9203484110a75fc49ef21027abc02d6888d
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2018-12-01 00:08:01 +0300

    Initial Commit

---
+++ Quotia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quotia
+//
+//  Created by Nikita Teslyuk on 01/12/2018.
+//  Copyright © 2018 Nikita Teslyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
