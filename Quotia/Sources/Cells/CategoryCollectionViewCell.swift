d0349480153fd55ff96e215a868dd4bd87835c65
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2018-12-01 23:23:28 +0300

    Cell is designed.

---
+++ Quotia/Sources/Cells/CategoryCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Quotia
+//
+//  Created by Nikita Teslyuk on 01/12/2018.
+//  Copyright © 2018 Nikita Teslyuk. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryName: UILabel!
+}
