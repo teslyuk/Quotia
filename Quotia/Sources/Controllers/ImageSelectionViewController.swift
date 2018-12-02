6329c874ed3cf686b8081baa58d6bd99ddeb6d3c
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2018-12-02 21:54:01 +0300

    Added app Icons

--- Quotia/Sources/Controllers/ImageSelectionViewController.swift
+++ Quotia/Sources/Controllers/ImageSelectionViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ImageSelectionViewController: UIViewController {
 
+    var image: UIImage?
+    var category: Category?
+    
+    @IBOutlet weak var initialImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let availableImage = image, let availableCategory = category {
+            
+            initialImageView.image = availableImage
+            categoryLabel.text = availableCategory.categoryName
+            
+        }
+        
     }
 
 }
