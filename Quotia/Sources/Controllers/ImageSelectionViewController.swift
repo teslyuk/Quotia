3c811a11eecdfa15727cc013e7b84b5b12133a8a
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2018-12-02 20:50:27 +0300

    Created new VC.

---
+++ Quotia/Sources/Controllers/ImageSelectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSelectionViewController.swift
+//  Quotia
+//
+//  Created by Nikita Teslyuk on 02/12/2018.
+//  Copyright © 2018 Nikita Teslyuk. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
