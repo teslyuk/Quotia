f5d8a5eb0d63afcfd62ab6f66f3b92bb1967ff61
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2018-12-06 00:12:25 +0300

    add new app icons and improved autolayout settings (not for the iPhone X+ models)

---
+++ Quotia/Sources/Views/PhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoView.swift
+//  Quotia
+//
+//  Created by Nikita Teslyuk on 05/12/2018.
+//  Copyright © 2018 Nikita Teslyuk. All rights reserved.
+//
+
+import UIKit
+
+class PhotoView: UIView {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var photographerLabel: UILabel!
+}
